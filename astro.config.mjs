f7051effcc6d41fabaccacf703892a9df5ba3b05
Rafael-Pelayo-afk <rafaelpelayosanchez@gmail.com>
2026-09-11 18:15:06 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwindcss from '@tailwindcss/vite';
 import icon from 'astro-icon';
 
 export default defineConfig({
+  site: 'https://Rafael-Pelayo-afk.github.io',
   output: 'static',
   vite: {
     plugins: [tailwindcss()],
